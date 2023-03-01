74150392ac1a5c6b3ad54fb28ae2499c0f813a90
Forostin <alex73foros@gmail.com>
2023-03-01 15:16:14 +0200

    Added component CartEmpty

---
+++ src/components/CartEmpty.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import cartEmptyPng from '../assets/img/empty-cart.png';
+const CartEmpty = ()=>{
+    return (
+        <>
+        <div className="cart cart--empty">
+           <h2>Корзина пустая</h2>
+           <img src={cartEmptyPng} alt="Empty Cart"></img>
+           <Link to ="/" className="button button--outline button--add go-back-btn">
+                  <span>Вернуться на главную страницу</span>
+           </Link>
+        </div>
+        </>
+    )
+}
+
+export default CartEmpty;
